a4c4d8745c886b87e20f7fb7bd62c95f912ebdd0
Prabhas Kurapati <prabhask@berkeley.edu>
2026-02-12 11:39:19 -0500

    [DEV] update precommit hook

--- static/sw.js
+++ static/sw.js
@@ -41,7 +41,7 @@
  */
 
 /** Unique version identifier, auto-updated on each build by the Vite plugin. */
-const APP_VERSION = 'mljojj71';
+const APP_VERSION = 'mljoozxq';
 
 /* ============================================================
    Cache Bucket Names
